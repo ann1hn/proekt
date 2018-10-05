62ed4bf3eb377ff0095bf3ad2b4b96230ca9ddfc
ann1hn <hannah19111999@gmail.com>
2018-10-05 12:31:53 +0300

    lala123

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+   <head>
+      <title> название </title>
+	  <meta charset="utf-8">
+	  <meta name="description" content="1-2-3  предложения (о сайте)">
+	  <meta name="keywords" content="не больше 125 символов (каталог, подарки и т.д.(через запятую))">
+	  <link type="text/css" rel="stylesheet" href="/media/bootstrap/css/bootstrap.min.css" />
+	  <link type="text/css" rel="stylesheet" href="/media/css/style.css" />
+	  
+	
+</head>
+
+ 
+   <body>
+      <div class="header" >
+	   <img id="logo" src="/media/img/logo.png"/> 
+	   <h1 class="logotext"> Название проекта
+	   </h1>
+	  </div>
+	  <nav class="topmenu">
+      <a href="#"> Главная 
+	  <a href="#"> Новости </a>
+	  </nav>
+     <div class="container">
+	 <div class="col-md-2">
+	 Меню
+	 </div>
+	 <div class="col-md-8">
+	 <h2> Добро пожаловать на сайт! </h2>
+	 Приветствие
+	 <div class="col-md-2">
+	 Виджеты
+	 </div>
+	 </div>
+</html>
